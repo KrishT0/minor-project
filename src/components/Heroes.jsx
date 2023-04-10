649f1f2c6bc0dddfbec3e75e6c0d9a575c71cc4f
Krishna-windows <krshnabiswal619@gmail.com>
2023-04-10 06:32:01 +0530

    updated readme

--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -3,7 +3,7 @@ export default function Heroes() {
           <div className="heroes flex">
                <img src="heroes-left.jpg"  className="photo-left"/>
                <div className="text-right flex">
-                    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque minima temporibus, sit a mollitia quis nostrum impedit omnis recusandae. Delectus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga incidunt dolore a quos iste mollitia amet laudantium asperiores sunt magnam nesciunt dolorem est quasi eligendi blanditiis quia vero, quibusdam quidem?</h2>
+                    <h2>Discover your dream college with our advanced recommendation system. Our sophisticated algorithm matches your unique interests and goals with top institutions to help you find the perfect fit. Join the ranks of successful students and start your journey to greatness today. With our system, finding the right college has never been easier. Our comprehensive database includes a wide range of colleges and universities, so you can be sure to find the one that's right for you. Don't leave your future to chance - let us help you find your path to success</h2>
                </div>
           </div>
      )
