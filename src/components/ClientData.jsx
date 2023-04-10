61df03a77c36463ec1bae8b012132015d39ad020
Krishna-windows <krshnabiswal619@gmail.com>
2023-04-10 14:07:42 +0530

    media-queries added

--- src/components/ClientData.jsx
+++ src/components/ClientData.jsx
@@ -1,19 +1,19 @@
 export default function Client() {
      return (
-          <div className="client flex">
-               <div className=" data flex">
+          <div className="client ">
+               <div className=" data d1 flex">
                     <h2>Students Connected</h2>
                     <h2>10K+</h2>
                </div>
-               <div className=" data flex">
+               <div className=" data d2 flex">
                     <h2>College Connected</h2>
                     <h2>500+</h2>
                </div>
-               <div className=" data flex">
+               <div className=" data d3 flex">
                     <h2>Global Community</h2>
                     <h2>12+</h2>
                </div>
-               <div className=" data flex">
+               <div className=" data d4 flex">
                     <h2>Site Visit</h2>
                     <h2>0</h2>
                </div>
